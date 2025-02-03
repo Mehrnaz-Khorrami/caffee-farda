9f3d6977570403aa9b422ecb2027abd98e425b1a
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-02-03 19:34:29 +0330

    feat: main page component and set background and logo images

---
+++ app/components/mainPage.vue
@@ -0,0 +1,31 @@
+<script setup>
+const {mobile} = useDisplay()
+</script>
+
+<template>
+  <v-container fluid class="bgStyle fill-height  d-flex align-center justify-center">
+    <div class="text-center">
+      <div class>
+        <v-img
+            src="/img/farda-logo-04.png"
+            alt="font-logo"
+            :width="mobile ? '350': '500'"
+            eager
+        >
+        </v-img>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<style>
+.bgStyle {
+  background-image: url('/img/background.webp') !important;
+  background-size: cover !important;
+  background-position: center !important;
+  background-repeat: no-repeat !important;
+  background-attachment: fixed !important;
+}
+
+
+</style>
