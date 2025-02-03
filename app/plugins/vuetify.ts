79c0ad8ba70a3effbbc08260fd1a9c9b7bb3980d
Mehrnaz-Khorrami <mehrnazkhorrami769@gmail.com>
2025-02-03 21:37:33 +0330

    chore: config my custom config for vuetify

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -1,32 +1,8 @@
-import {createVuetify} from "vuetify";
-import { fa } from "vuetify/locale";
-
+export default defineNuxtPlugin((nuxtApp) => {
   // check https://vuetify-nuxt-module.netlify.app/guide/nuxt-runtime-hooks.html
-  export default defineNuxtPlugin((app) => {
-    const vuetify = createVuetify({
-      // ... your configuration
-      theme: {
-        defaultTheme: 'myCustomTheme',
-        themes: {
-          myCustomTheme: {
-            dark: false, // Set to true if you want a dark theme
-            colors: {
-              background: '#E5E1DA',
-            },
-          },
-        },
-      },
-      locale: {
-        locale: "fa",
-        messages: { fa },
-        rtl: {
-          fa: true,
-        },
-      },
-      styles: {
-        configFile: "app/assets/settings.scss",
-      },
-
-    })
-    app.vueApp.use(vuetify)
+  nuxtApp.hook('vuetify:before-create', (options) => {
+    if (import.meta.client) {
+      console.log('vuetify:before-create', options)
+    }
   })
+})
--- vuetify.config.ts
+++ vuetify.config.ts
@@ -1,8 +1,21 @@
 import { defineVuetifyConfiguration } from 'vuetify-nuxt-module/custom-configuration'
+import {fa} from "vuetify/locale";
 
 export default defineVuetifyConfiguration({
   // your Vuetify options here
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'fardaCaffee',
+    themes: {
+      fardaCaffee: {
+        dark: false,
+      }
+    }
   },
+  locale:{
+    locale: "fa",
+    messages: {fa},
+    rtl: {
+      fa: true,
+    }
+  }
 })
